88031fcb5c629ac32ca4dc95a57aae73c15d6901
Hubert Furmanek <hubert@Furmi-MacBook-Pro.local>
2022-03-01 22:56:23 +0000

    Initial commit

---
+++ PrintDiamond/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrintDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
