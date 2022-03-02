e002dcf3813513d00ebf1582755735517c7dabd4
Hubert Furmanek <furmanek.hubert@gmail.com>
2022-03-02 13:53:41 +0000

    Correct test case output for Z character

---
+++ PrintDiamond.UnitTests/DiamondStringBuilder/TestHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.DotNet.InternalAbstractions;
+
+namespace PrintDiamond.UnitTests.DiamondStringBuilder
+{
+    public class TestHelper
+    {
+        public static string GetTestDataFolder(string testDataFolder)
+        {
+            string startupPath = ApplicationEnvironment.ApplicationBasePath;
+            var pathItems = startupPath.Split(Path.DirectorySeparatorChar);
+            var pos = pathItems.Reverse().ToList().FindIndex(x => string.Equals("bin", x));
+            string projectPath = String.Join(Path.DirectorySeparatorChar.ToString(), pathItems.Take(pathItems.Length - pos - 1));
+
+            return Path.Combine(projectPath, "Test_Data", testDataFolder);
+        }
+    }
+}
