d10c88692b5a4d3518593d2e13f93efc213bb972
Hubert Furmanek <furmanek.hubert@gmail.com>
2022-03-02 09:38:30 +0000

    Add unit tests for DiamondStringBuilder

---
+++ PrintDiamond.UnitTests/DiamondStringBuilder/DiamondStringBuilderTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace PrintDiamond.UnitTests.DiamondStringBuilder
+{
+    [TestFixture]
+    [Category("GetDiamondText")]
+    public class DiamondStringBuilderTest
+    {
+        private readonly PrintDiamond.DiamondStringBuilder _diamondStringBuilder = new();
+
+        [TestCase('1')]
+        [TestCase('!')]
+        [TestCase('Æ')]
+        public void InvalidInputCharacterThrow(char character)
+        {
+            Assert.Throws<ArgumentException>(() => _diamondStringBuilder.GetDiamondText(character));
+        }
+
+        [TestCase('A', ExpectedResult = 1*2)] // w x h
+        [TestCase('B', ExpectedResult = 3*4)]
+        [TestCase('C', ExpectedResult = 5*6)]
+        [TestCase('Z', ExpectedResult = (1+2*25)*(2*26))]
+        public int CorrectLength(char character)
+        {
+            var result = _diamondStringBuilder.GetDiamondText(character);
+            return result.Length;
+        }
+
+        [TestCase('A', ExpectedResult = 1)]
+        [TestCase('B', ExpectedResult = 3)]
+        [TestCase('C', ExpectedResult = 5)]
+        [TestCase('Z', ExpectedResult = 1 + 2 * 25)]
+        public int CorrectNumberOfRows(char character)
+        {
+            var result = _diamondStringBuilder.GetDiamondText(character);
+            return result.Count(s => s == CharAlphhabeticHelper.NEW_LINE);
+        }
+
+        [TestCase('A')]
+        public void CorrectNumberOfCharacterA(char character)
+        {
+            var result = _diamondStringBuilder.GetDiamondText(character);
+
+            Assert.AreEqual(1, result.Count(s => s == character));
+        }
+
+        [TestCase('B')]
+        [TestCase('C')]
+        [TestCase('Z')]
+        public void CorrectNumberOfCharacters(char character)
+        {
+            var aChar = 'A';
+            int firstCharUnicode = aChar,
+                currentCharUnicode = character;
+
+            var result = _diamondStringBuilder.GetDiamondText(character);
+
+            Assert.AreEqual(2, result.Count(s => s == aChar), $"Wrong number of character {aChar}");
+            for (var i = firstCharUnicode + 1; i <= currentCharUnicode; i++)
+            {
+                Assert.AreEqual(4, result.Count(s => s == (char)i), $"Wrong number of character {(char)i}");
+            }
+        }
+
+        [TestCase('A')]
+        [TestCase('B')]
+        [TestCase('C')]
+        [TestCase('Z')]
+        public void CorrectDiamondStructure(char character)
+        {
+            var expected = File.ReadAllText(Path.Combine(TestContext.CurrentContext.TestDirectory, "DiamondStringBuilder", $"CorrectDiamondStructure{character}.txt"));
+
+            var result = _diamondStringBuilder.GetDiamondText(character);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
--- PrintDiamond/Utils/CharAlphabeticHelper.cs
+++ PrintDiamond/Utils/CharAlphabeticHelper.cs
@@ -1,8 +1,11 @@
-﻿namespace PrintDiamond
+﻿using System;
+
+namespace PrintDiamond
 {
     public static class CharAlphhabeticHelper
     {
         public static readonly int A_LETTER_UNICODE = 'A';
         public static readonly char[] ALPHABET_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        public static readonly char NEW_LINE = '\n';
     }
 }
