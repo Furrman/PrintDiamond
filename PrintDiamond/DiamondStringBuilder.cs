e525f4b62a297dca7a524a2a537f2b9d8443ed77
Hubert Furmanek <furmanek.hubert@gmail.com>
2022-03-02 09:51:56 +0000

    Add input validation for DiamondStringBuilder

--- PrintDiamond/DiamondStringBuilder.cs
+++ PrintDiamond/DiamondStringBuilder.cs
@@ -6,7 +6,10 @@ namespace PrintDiamond
     {
         public string GetDiamondText(char lastLetter)
         {
-
+            if (!CharAlphhabeticHelper.IsAlphabethic(lastLetter))
+            {
+                throw new ArgumentException("Last character for diamond has to be a letter.");
+            }
 
             return string.Empty;
         }
--- PrintDiamond/Utils/CharAlphabeticHelper.cs
+++ PrintDiamond/Utils/CharAlphabeticHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace PrintDiamond
 {
@@ -7,5 +8,11 @@ namespace PrintDiamond
         public static readonly int A_LETTER_UNICODE = 'A';
         public static readonly char[] ALPHABET_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
         public static readonly char NEW_LINE = '\n';
+
+        public static bool IsAlphabethic(char character)
+        {
+            Regex rg = new(@"^[a-zA-Z]$");
+            return rg.IsMatch(char.ToString(character));
+        }
     }
 }
