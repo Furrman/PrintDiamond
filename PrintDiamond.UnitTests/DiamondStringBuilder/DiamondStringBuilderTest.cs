0ce541ecf7f95e667a77565863e4960166644154
Hubert Furmanek <furmanek.hubert@gmail.com>
2022-03-02 09:42:54 +0000

    Add test cases for lower case characters

--- PrintDiamond.UnitTests/DiamondStringBuilder/DiamondStringBuilderTest.cs
+++ PrintDiamond.UnitTests/DiamondStringBuilder/DiamondStringBuilderTest.cs
@@ -19,10 +19,14 @@ namespace PrintDiamond.UnitTests.DiamondStringBuilder
             Assert.Throws<ArgumentException>(() => _diamondStringBuilder.GetDiamondText(character));
         }
 
-        [TestCase('A', ExpectedResult = 1*2)] // w x h
-        [TestCase('B', ExpectedResult = 3*4)]
-        [TestCase('C', ExpectedResult = 5*6)]
-        [TestCase('Z', ExpectedResult = (1+2*25)*(2*26))]
+        [TestCase('A', ExpectedResult = 1 * 2)] // w x h
+        [TestCase('a', ExpectedResult = 1 * 2)] // w x h
+        [TestCase('B', ExpectedResult = 3 * 4)]
+        [TestCase('b', ExpectedResult = 3 * 4)]
+        [TestCase('C', ExpectedResult = 5 * 6)]
+        [TestCase('c', ExpectedResult = 5 * 6)]
+        [TestCase('Z', ExpectedResult = (1 + 2 * 25) * (2 * 26))]
+        [TestCase('z', ExpectedResult = (1 + 2 * 25) * (2 * 26))]
         public int CorrectLength(char character)
         {
             var result = _diamondStringBuilder.GetDiamondText(character);
@@ -30,9 +34,13 @@ namespace PrintDiamond.UnitTests.DiamondStringBuilder
         }
 
         [TestCase('A', ExpectedResult = 1)]
+        [TestCase('a', ExpectedResult = 1)]
         [TestCase('B', ExpectedResult = 3)]
+        [TestCase('b', ExpectedResult = 3)]
         [TestCase('C', ExpectedResult = 5)]
+        [TestCase('c', ExpectedResult = 5)]
         [TestCase('Z', ExpectedResult = 1 + 2 * 25)]
+        [TestCase('z', ExpectedResult = 1 + 2 * 25)]
         public int CorrectNumberOfRows(char character)
         {
             var result = _diamondStringBuilder.GetDiamondText(character);
@@ -40,21 +48,25 @@ namespace PrintDiamond.UnitTests.DiamondStringBuilder
         }
 
         [TestCase('A')]
+        [TestCase('a')]
         public void CorrectNumberOfCharacterA(char character)
         {
             var result = _diamondStringBuilder.GetDiamondText(character);
 
-            Assert.AreEqual(1, result.Count(s => s == character));
+            Assert.AreEqual(1, result.Count(s => s == char.ToUpper(character)));
         }
 
         [TestCase('B')]
+        [TestCase('b')]
         [TestCase('C')]
+        [TestCase('c')]
         [TestCase('Z')]
+        [TestCase('z')]
         public void CorrectNumberOfCharacters(char character)
         {
             var aChar = 'A';
             int firstCharUnicode = aChar,
-                currentCharUnicode = character;
+                currentCharUnicode = char.ToUpper(character);
 
             var result = _diamondStringBuilder.GetDiamondText(character);
 
@@ -66,14 +78,19 @@ namespace PrintDiamond.UnitTests.DiamondStringBuilder
         }
 
         [TestCase('A')]
+        [TestCase('a')]
         [TestCase('B')]
+        [TestCase('b')]
         [TestCase('C')]
+        [TestCase('c')]
         [TestCase('Z')]
+        [TestCase('z')]
         public void CorrectDiamondStructure(char character)
         {
-            var expected = File.ReadAllText(Path.Combine(TestContext.CurrentContext.TestDirectory, "DiamondStringBuilder", $"CorrectDiamondStructure{character}.txt"));
+            var upperChar = char.ToUpper(character);
+            var expected = File.ReadAllText(Path.Combine(TestContext.CurrentContext.TestDirectory, "DiamondStringBuilder", $"CorrectDiamondStructure{upperChar}.txt"));
 
-            var result = _diamondStringBuilder.GetDiamondText(character);
+            var result = _diamondStringBuilder.GetDiamondText(upperChar);
 
             Assert.AreEqual(expected, result);
         }
